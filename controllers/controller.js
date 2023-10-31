7a9ab8ac01126f2987d5eaac952ef44d135c9df0
Mukhtar <mukhtarabdulfatai@gmail.com>
2023-10-31 14:38:49 +0100

    added token

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,5 @@
 import Web3 from "web3";
+import jwt from "jsonwebtoken";
 import "dotenv/config";
 const web3 = new Web3('https://rpc.sepolia.org/');
 
@@ -6,6 +7,32 @@ const privateKey = '264911a6fe288981cdf18567468ed66f985322919c3055e5ad45754cb974
 const account = web3.eth.accounts.privateKeyToAccount(`0x${privateKey}`);
 const gasLimit = web3.utils.toHex(24856); 
 const gasPrice = web3.utils.toWei('20', 'gwei')
+
+export const createToken = async (req, res) =>{
+    try {
+        const {name, authcode} = req.body;
+
+        const token = jwt.sign(
+            { name: name, authcode: authcode},
+            process.env.JWT_TOKEN_KEY,
+            {
+              expiresIn: "365d",
+            }
+          );
+
+          return res.status(200).json({
+            status: false,
+            message: "token creation successful",
+            token: token
+            })
+
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            message: "An error occured" + error
+            })
+    }
+}
 
 export const getFingerPrintDetails = async ( req, res ) => {
     try {
@@ -29,6 +56,12 @@ export const getFingerPrintDetails = async ( req, res ) => {
             .catch(error => {
               console.error('Transaction error:', error);
             });
+
+            return res.status(200).json({
+                status: false,
+                message: "sent to the blockchain successfully"
+                })
+
     } catch (error) {
     return res.status(500).json({
         status: false,
--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { 
     getFingerPrintDetails,
-    sendFingerPrintDetails
+    sendFingerPrintDetails,
+    createToken
  } from "../controllers/controller.js";
  import { verifyToken } from "../middlewares/auth.js"
 const router = Router();
 
 router.route("/fingerprint-details").post(verifyToken, getFingerPrintDetails);
 router.route("/fingerprint-details").get(sendFingerPrintDetails);
+router.route("/create-token").post(createToken);
 
 export default router;
