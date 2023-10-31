f9d9effafc740fc74397aefff90a3070ff0d0243
Mukhtar <mukhtarabdulfatai@gmail.com>
2023-10-31 14:06:57 +0100

    added verifyToken

--- routes/route.js
+++ routes/route.js
@@ -3,9 +3,10 @@ import {
     getFingerPrintDetails,
     sendFingerPrintDetails
  } from "../controllers/controller.js";
+ import { verifyToken } from "../middlewares/auth.js"
 const router = Router();
 
-router.route("/fingerprint-details").post(getFingerPrintDetails);
+router.route("/fingerprint-details").post(verifyToken, getFingerPrintDetails);
 router.route("/fingerprint-details").get(sendFingerPrintDetails);
 
 export default router;
