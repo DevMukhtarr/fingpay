1afec9d59a412fbb847f1d01fd285e3e95220cba
Mukhtar <mukhtarabdulfatai@gmail.com>
2023-10-31 13:42:49 +0100

    added private key to env

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,7 @@
 import Web3 from "web3";
 const web3 = new Web3('https://rpc.sepolia.org/');
-// const web3 = new Web3('https://rpc2.sepolia.org');
 
-const privateKey = "264911a6fe288981cdf18567468ed66f985322919c3055e5ad45754cb974353e";
+const privateKey = process.env.PRIVATE_KEY;
 const account = web3.eth.accounts.privateKeyToAccount(`0x${privateKey}`);
 const gasLimit = web3.utils.toHex(24856); 
 const gasPrice = web3.utils.toWei('20', 'gwei')
